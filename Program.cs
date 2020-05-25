2b16f8b588a2d7cb7e134e1a83a5287bd3a50aa7
Micke <micke@nevyn.se>
2020-05-25 08:37:01 +0200

    FizzBuzz

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int FIZZ = 3;
+            const int BUZZ = 5;
+
+            for(int i = 1; i <= 100; i++){
+                var output = "";
+
+                if(i % FIZZ == 0)
+                    output += "Fizz";
+                if(i % BUZZ == 0)
+                    output += "Buzz";
+                
+                if(output == "")
+                    output = i.ToString();
+                
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
